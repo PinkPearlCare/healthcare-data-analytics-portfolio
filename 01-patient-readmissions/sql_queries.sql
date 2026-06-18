8aba7074287eba9ef95ca42da05557ec28dbae68
DovileAnalytics <woofyourway19@gmail.com>
2026-06-18 04:37:24 -0400

    Add SQL queries for patient readmissions project

---
+++ 01-patient-readmissions/sql_queries.sql
@@ -0,0 +1,17 @@
+-- Patient Readmissions Project
+
+-- View all patients
+SELECT *
+FROM patients;
+
+-- View patient age groups
+SELECT age_group
+FROM patients;
+
+-- View diagnosis information
+SELECT diagnosis
+FROM patients;
+
+-- View readmission status
+SELECT readmitted
+FROM patients;
